bb9ae096d492d573c81c1f7f278ed6675e1ff246
Shoumik Chowdhury <shoumikdc@gmail.com>
2024-08-05 19:30:25 -0400

    Fix init for single_charge_transmon

--- qcsys/devices/__init__.py
+++ qcsys/devices/__init__.py
@@ -8,7 +8,6 @@ from .resonator import *
 from .kno import *
 from .transmon import *
 from .tunable_transmon import *
-from .transmon_single_charge_basis import *
 from .fluxonium import *
 from .ats import *
 from .ideal_qubit import *
